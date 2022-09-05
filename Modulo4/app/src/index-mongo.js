dc38d7156335711a94b3904bc7537494bc117a87
Erick Andrew Bustamante Flores <72901697+ErickABF@users.noreply.github.com>
2022-09-05 17:14:32 -0500

    Add files via upload

---
+++ Modulo4/app/src/index-mongo.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ProfileRouter = require("./routes/profiles");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api", ProfileRouter);
+
+mongoose.connect(
+  "mongodb://mongo/example",
+  { useNewUrlParser: true },
+  (err, res) => {
+    err && console.log(`ERROR: Connecting to DB ${err}`);
+    app.listen(3000, () =>
+      console.log("Node server running on http://mongo:3000")
+    );
+  }
+);
---
+++ Modulo4/app/src/routes/profiles.js
@@ -0,0 +1,18 @@
+const ProfileController = require("../controllers/profiles");
+const express = require("express");
+
+// API routes
+const router = express.Router();
+
+router.get("/all", ProfileController.findAllProfiles)
+router
+  .route("/profiles")
+  .get(ProfileController.findAllProfiles)
+  .post(ProfileController.addProfile);
+
+router
+  .route("/profiles/:id")
+  .get(ProfileController.findById)
+  .delete(ProfileController.deleteProfile);
+
+module.exports = router;
